0121e1e2d162b455c8e7f7871ac4c4813398a546
Luis Rojas <chikavi@Santiagos-Mac-mini-2.local>
2019-01-07 12:28:59 -0600

    agregue un layout del footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,45 @@
+  <footer class="page-footer footer-color">
+          <div class="container">
+            <div class="row">
+              <div class="col m4 s12">
+                <h5 class="footer-text-color">ComiCUT</h5>
+                <p class="  udg-text">Esta pagina fue creada para los estudiantes que dia a dia se ganan su propio dinero vendiendo comida en el centro universitario de Tonala.</p>
+              </div>
+              <div class="col m4 s6 ">
+                <h5 class="footer-text-color ">Contenido</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Preguntas y Respuestas</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Acerca de Comicut</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Contactar al creador</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Tutorial</a></li>
+                </ul>
+              </div>
+              <div class="col m4 s6 ">
+                <h5 class="footer-text-color ">Siguenos</h5>
+                <ul>
+                  <li>
+                    <a class="valign-wrapper" href="https://twitter.com/Cutonala_udg"><i class="fab fa-twitter fa-2x"></i>&nbsp;&nbsp; &nbsp;&nbsp;Twitter</a>
+                  </li>
+                  <li>
+                    <a class="valign-wrapper" href="https://www.facebook.com/centrouniversitariodetonala/">
+                      <i class="fab fa-facebook fa-2x fb"></i>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;Facebook</a>
+                    
+                  </li>
+                  <li>
+                    <a class="yt valign-wrapper" href="https://www.youtube.com/channel/UCdAVkoHuKAMWM3yaOW_uZPQ"><i class="fab fa-youtube fa-2x yt"></i>   &nbsp;&nbsp;&nbsp; Youtube</a>
+                  </li>
+                  <li>
+                  <a class="im valign-wrapper " href="https://www.instagram.com/cutonala.udg/"><i class="fab fa-instagram fa-2x im"></i> &nbsp;&nbsp; &nbsp;&nbsp;Instagram</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            <center class="footer-text-color">
+              © 2018 Chikavi - Luis Rojas -
+            </center>
+            </div>
+          </div>
+        </footer>
