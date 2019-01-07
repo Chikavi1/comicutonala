5328f2300e6a183040536907fc9a8c451644b353
Luis Rojas <chikavi@Santiagos-Mac-mini-2.local>
2019-01-07 12:28:23 -0600

    agregue la vista de las categorias

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+@section('content')
+<style>
+	.image-max{
+		height: 12em !important;
+	}
+</style>
+<div class="row">
+	<div class="col m3 animated p2 bounceIn">
+		
+      <div class="card-panel teal">
+        <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+        I am convenient because I require little markdown to use effectively. I am similar to what is called a panel in other frameworks.
+        </span> 
+  </div>
+	</div>	
+	
+	<div class=" col m9">
+		<div class="m3 col  ">
+			<a href="/">
+				
+			<div class="card " >
+		        <div class="card-image">
+		          <img class="image-max" src="{{ asset('img/salado.jpg') }}">
+		        	  <p class=" card-title "  >Comida</p>
+		        </div>
+      		</div>
+			</a>
+		</div>
+
+		<div class="m3 col ">
+			<div class="card">
+		        <div class="card-image ">
+		          <img class="image-max" src="{{ asset('img/postres.jpg') }}" >
+		        	  <p class=" card-title "  >Postres</p>
+		        </div>
+      		</div>
+		</div>
+		<div class="m3 col ">
+			<div class="card">
+		        <div class="card-image">
+		          <img class="image-max" src="{{ asset('img/bebidas.jpg') }}">
+		        	  <p class=" card-title "  >Bebidas</p>
+		        </div>
+      		</div>
+		</div>
+		<div class="m3 col ">
+			<div class="card">
+		        <div class="card-image">
+		          <img class="image-max" src="{{ asset('img/dulces.jpg') }}">
+		        	  <p class=" card-title "  >Dulces </p>
+		        </div>
+      		</div>
+		</div>
+		<div class="m3 col ">
+			<div class="card">
+		        <div class="card-image">
+		          <img class="image-max" src="https://materializecss.com/images/sample-1.jpg">
+		        	  <p class=" card-title "  >Categoria </p>
+		        </div>
+      		</div>
+		</div>
+	</div>
+	
+
+
+  </div>
+
+
+@endsection
