6de9556ef952556c2602a0346ee36697fe34699f
chikavi1 <chikavi@hotmail.com>
2019-09-17 22:53:45 -0500

    prueba app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <!-- Styles -->
     <link href="{{ asset('css/materialize.min.css') }}" rel="stylesheet"></link>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
     
     <meta name="theme-color" content="#17202f" >
